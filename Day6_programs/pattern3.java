8d50b4cba315ce47779ffcdfb41ffc0116417dd6
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2021-12-29 20:35:53 +0530

    first commit

---
+++ Day6_programs/pattern3.java
@@ -0,0 +1,28 @@
+//program to print Hollow Half Pyramid
+import java.util.Scanner;
+public class Pattern
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+    {
+                if(i==n || j==i || j==1)
+                {
+                   System.out.print(j+ " ");       
+                }
+              else
+              {
+                  System.out.print("  ");
+              }
+        
+    }
+            System.out.println();
+            
+        }
+    }
+    }
+
