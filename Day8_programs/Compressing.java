d9a77e07f5f6d3798f0c7585a448ce61ebf0c211
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2021-12-30 19:13:35 +0530

    first commit

---
+++ Day8_programs/Compressing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Compressing {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 3, 1, 2, 4 };
+        String str = sc.nextLine();
+        for (int i = arr[0]; i <= arr[0]; i++) 
+        {
+            for (int j = arr[1]; j <= arr[1]; j++) 
+            {
+                for (int k = arr[2]; k <= arr[2]; k++) 
+                {
+                    for (int l = arr[3]; l <= arr[3]; l++) 
+                    {
+                        System.out.print(sc.nextLine() + arr[0] + sc.nextLine() + arr[1] + sc.nextLine() + arr[2] + sc.nextLine()
+                                        + arr[3]);
+                    }
+
+                    System.out.println(" ");
+                }
+            }
+        }
+
+    }
