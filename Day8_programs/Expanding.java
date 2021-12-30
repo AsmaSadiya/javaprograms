1bf9bb865ca751c63b9403d6fb09eee7577f061d
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2021-12-29 23:06:03 -0500

    first commit

---
+++ Day8_programs/Expanding.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Expanding
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int l=sc.nextInt();
+        sc.nextLine();
+        String str=sc.nextLine();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print("a");
+        }  
+            for(int j=1;j<=m;j++)
+         {
+             System.out.print("c");
+
+         }
+          for(int k=1;k<=l;k++)
+          {
+              System.out.print("t");
+          }
+          System.out.println(" ");
+        }
+              
+          }
+        
