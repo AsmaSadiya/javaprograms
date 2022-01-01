fb1c978385b8705e92edc90e6130e7e92190c306
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 19:32:09 +0530

    first commit

---
+++ Day10_programs/file3.java
@@ -0,0 +1,17 @@
+//program to print index
+import java.util.Scanner;
+
+class File3
+
+{
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = "clock";
+        System.out.println(str.indexOf("c"));
+        System.out.println(str);
+
+    }
+
+}
+
