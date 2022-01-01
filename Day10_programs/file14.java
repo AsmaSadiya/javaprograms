bd1c84add8e9c134f237ba91cd1bacbdc412f4e3
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 20:23:40 +0530

    first commit

---
+++ Day10_programs/file14.java
@@ -0,0 +1,12 @@
+//program to print into lowercase
+public class File14 {
+
+    public static void main(String args[]) {
+        String str = " PRogramming LAnguages";
+        String str1 = "jAVA LANGU@GE";
+        String s = str.toLowerCase();
+        String n = str1.toLowerCase();
+        System.out.println(s);
+        System.out.println(n);
+    }
+}
