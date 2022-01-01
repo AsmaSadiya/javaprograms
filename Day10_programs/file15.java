82e8cf4235b5143d3bb83495dd58cb58409cbb80
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 20:25:03 +0530

    first commit

---
+++ Day10_programs/file15.java
@@ -0,0 +1,12 @@
+//program to check a string contains a char/string
+public class File16 {
+    public static void main(String args[]) {
+        String input = "visual studio code";
+        String string = "random access";
+        String set = "powershell";
+        System.out.println(string.contains("ran"));
+        System.out.println(input.contains("code"));
+        System.out.println(set.contains("ell"));
+    }
+
+}
