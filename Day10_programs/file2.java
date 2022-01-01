a656be5ebc8707024f8f4b8280170a904818e740
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 19:30:43 +0530

    first commit

---
+++ Day10_programs/file2.java
@@ -0,0 +1,12 @@
+//program to concat a string
+import java.util.Scanner;
+public class File2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String s = sc.nextLine();
+        System.out.println(str1.concat(s));
+
+    }
+
+}
