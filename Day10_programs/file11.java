914034397d65170fe973979227f22800cb70c9c5
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 20:20:12 +0530

    first commit

---
+++ Day10_programs/file11.java
@@ -0,0 +1,11 @@
+//program to print replace a string/char
+public class File11 {
+    public static void main(String args[]) {
+        String std = "aaeemooaaea";
+        String str = "bbeeekkhhuuh";
+        System.out.println(std.replace("aa", "uu"));
+        System.out.println(str.replace("h", "g"));
+
+    }
+
+}
