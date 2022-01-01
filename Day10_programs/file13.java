c5b99a80871fdfe4576ffbf95b5fbbcf904101d3
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 20:22:40 +0530

    first commit

---
+++ Day10_programs/file13.java
@@ -0,0 +1,15 @@
+//program to check string isempty 
+public class File13_1 {
+    public static void main(String args[]) {
+        String input = "asdef";
+        String input1 = "34567894";
+        String input2 = "syedsadiya";
+        String input3 = "";
+        String input4 = "   ";
+        boolean i = input.isEmpty();
+        boolean k = input2.isEmpty();
+        boolean j = input3.isEmpty();
+        boolean l = input4.isEmpty();
+        System.out.println("i:" + i + " " + "k:" + k + " " + "j:" + j + " " + "l:" + l);
+    }
+}
