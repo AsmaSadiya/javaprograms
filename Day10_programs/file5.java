7ceef44b40f52eb54792aecfc3c3018de0feed41
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 19:35:45 +0530

    first commit

---
+++ Day10_programs/file5.java
@@ -0,0 +1,14 @@
+//program to perform trim method
+import java.util.Scanner;
+
+public class File5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = "  java programming  ";
+        String str1 = "   __this is a code__   ";
+        System.out.println(str.trim());
+        System.out.println(str1.trim());
+
+    }
+
+}
