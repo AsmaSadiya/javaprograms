5ffa237e887cd9a8e6673090a9f16b0db1ac9aeb
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-01 20:14:29 +0530

    first commit

---
+++ Day10_programs/file8.java
@@ -0,0 +1,10 @@
+//program to print length
+public class File8 {
+    public static void main(String args[]) {
+        String str = "java";
+        String arr[] = { "java", "is", "language" };
+        for (int i = 1; i <= str.length(); i++) {
+            System.out.println(str + " " + arr[2]);
+        }
+    }
+}
