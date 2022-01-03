0f1856ba67123f5403a4549660e532f6630d5ebd
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-03 02:14:52 -0500

    first commit

---
+++ Day1_programs/Java.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        String input="Hello World!";
+        String input1="Hello Java!";
+        System.out.println(input);
+        System.out.println(input1);
+    }
+}
