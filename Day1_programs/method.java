62a78d9da8020796e1bf25f965cedd0144e7da50
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-03 02:15:27 -0500

    first commit

---
+++ Day1_programs/method.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Method {
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner (System.in);
+        int n=input.nextInt();
+        double m=input.nextDouble();
+        input.nextLine();
+        String s=input.nextLine();
+        System.out.println("String:" +s);
+        System.out.println("Double:" +m);
+        System.out.println("Int:" +n);
+    }
+    
+}
