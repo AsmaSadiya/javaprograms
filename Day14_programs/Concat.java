79e2c2a0e8d6602dda80a5fefbadaa71429323bf
AsmaSadiya <93442597+AsmaSadiya@users.noreply.github.com>
2022-01-27 11:21:18 +0530

    first commit

---
+++ Day14_programs/Concat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class  Program6
+{
+    public static void main(String args[])
+    {
+
+    
+    Scanner scan=new Scanner(System.in);
+    
+    String s=scan.nextLine();
+    String s2=scan.nextLine();
+    System.out.println("Concat two strings:");
+    System.out.println(s + s2);
+    System.out.println("result is:"+ s+s2);
+}
+}
